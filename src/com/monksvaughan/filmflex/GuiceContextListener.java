fd709eef56c6cb04298acfc8d4f6676f15b4c6d6
Jon Vaughan <Jon Vaughan@192.168.1.102>
2010-05-29 15:27:06 +0100

    Renamed file

--- src/com/monksvaughan/filmflex/GuiceContextListener.java
+++ src/com/monksvaughan/filmflex/GuiceContextListener.java
@@ -4,7 +4,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
-public class MyGuiceServletContextListener extends GuiceServletContextListener {
+public class GuiceContextListener extends GuiceServletContextListener {
 
 	@Override
 	protected Injector getInjector() {
